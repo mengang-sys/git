430873b762433c96b6423183e728ec3323859d45
mengang-sys <377532225@qq.com>
2020-11-16 22:49:02 +0800

    Initial commit

---
+++ src/main/java/com/my/git/GitApplication.java
@@ -0,0 +1,14 @@
+package com.my.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class, args);
+        System.out.println("mengyang1");
+    }
+
+}
