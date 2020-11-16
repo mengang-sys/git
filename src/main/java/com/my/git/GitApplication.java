0f4c98837fce3a5ab7589225c49f7c01cd82c0a1
mengang-sys <377532225@qq.com>
2020-11-16 22:53:32 +0800

    aaa

--- src/main/java/com/my/git/GitApplication.java
+++ src/main/java/com/my/git/GitApplication.java
@@ -9,6 +9,7 @@ public class GitApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitApplication.class, args);
         System.out.println("mengyang1");
+        System.out.println("mengyang2");
     }
 
 }
